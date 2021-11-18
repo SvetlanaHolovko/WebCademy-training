965ca8a0a58521d965f723ab886d6748ccbb1e84
SvetlanaHolovko <svetta.gsn@gmail.com>
2021-11-18 10:23:44 +0200

    Updates burger-menu

--- eleganse-shop/js/main.js
+++ eleganse-shop/js/main.js
@@ -1,10 +1,10 @@
 // Фильтр на мобильных устройствах
-const sidebarToggle = document.querySelector('.menu-icon-wrapper');
-const menuIcon = document.querySelector('.menu-icon');
-const sidebar = document.querySelector('.nav__list-holder');
+const sidebarToggle = document.querySelector(".menu-icon-wrapper");
+const menuIcon = document.querySelector(".menu-icon");
+const sidebar = document.querySelector(".nav__list-holder");
 
 // Клик по кнопке для скрытия/показа фильтра и изменения иконки
 sidebarToggle.onclick = function () {
-    menuIcon.classList.toggle('menu-icon-active');
-    sidebar.classList.toggle('nav__list-holder--visible');
+  menuIcon.classList.toggle("menu-icon-active");
+  sidebar.classList.toggle("nav__list-holder--visible");
 };
